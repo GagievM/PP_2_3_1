e19ba5b23ba109f4ad6c27b1eaa3187d7647afc1
Magomed <gagiev_maga1999@mail.ru>
2024-12-06 21:21:36 +0300

    Done

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -5,20 +5,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import web.service.CarService;
 import web.service.impl.CarServiceImp;
 
 @Controller
 public class CarController {
 
-    private final CarServiceImp carServiceImp;
+    private final CarService carService;
 
     @Autowired
-    public CarController(CarServiceImp carServiceImp) {
-        this.carServiceImp = carServiceImp;
+    public CarController(CarService carService) {
+        this.carService = carService;
     }
     @GetMapping(value = "/cars")
     public String cars(@RequestParam(value = "count", defaultValue = "5") int count, ModelMap model) {
-        model.addAttribute("cars", carServiceImp.listCarsCount(count));
+        model.addAttribute("cars", carService.listCarsCount(count));
         return "cars";
     }
 }
