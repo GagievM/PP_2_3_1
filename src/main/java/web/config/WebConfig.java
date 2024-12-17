d2de8b372689a40a24a8be6b5c5f9b8e9d94d43a
Magomed <gagiev_maga1999@mail.ru>
2024-12-17 21:10:06 +0300

    done

--- src/main/java/web/config/WebConfig.java
+++ src/main/java/web/config/WebConfig.java
@@ -46,6 +46,5 @@ public class WebConfig implements WebMvcConfigurer {
         ThymeleafViewResolver resolver = new ThymeleafViewResolver();
         resolver.setTemplateEngine(templateEngine());
         registry.viewResolver(resolver);
-
     }
 }
